cbf508e7874d359fdc6e0ea99848cf627f0d277b
Stacy <aleyostacy@gmail.com>
2024-10-05 23:04:32 -0700

    added changes according to the instructions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
+
+const drivers = ["Stacy", "Aleyo", "Ochieng", "Stephanie", "Cleo", "Marchel"];
+
+console.log(findMatching(drivers, "Aleyo")); 
+
+console.log(fuzzyMatch(drivers, "A")); 
+
+const driverObjects = [
+  { name: "Stacy", hometown: "Nairobi" },
+  { name: "Aleyo", hometown: "Mombasa" },
+  { name: "Ochieng", hometown: "Nakuru" }
+];
+
+console.log(matchName(driverObjects, "Aleyo")); 
